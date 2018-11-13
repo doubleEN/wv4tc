1f8b85c2dc3d7b621e09ce260c04d8785ccdad96
Jx_M <813500697@qq.com>
2018-11-13 12:21:24 +0800

    增加向量模型测试接口

--- wv4tc/model_eval.py
+++ wv4tc/model_eval.py
@@ -8,13 +8,16 @@
 import pandas as pd
 from sklearn.metrics import *
 from wv4tc.feature_text import get_bow
-from wv4tc.utils import load_data, sbc2dbc
+from wv4tc.utils import load_data, sbc2dbc, load_vecs
 import pickle
 import os
 import jieba
 import logging
 
-log_file = "../log/test.log"
+# log_file = "../log/test_easenet_vec.log"
+# log_file = "../log/test_fudan_vec.log"
+log_file = "../log/test_sohu_vec.log"
+
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -25,7 +28,7 @@ logging.basicConfig(
 )
 
 
-def save_model(model, transfer, pkl_path):
+def save_model(pkl_path, model, transfer=None):
     """
     模型序列化
     :param model:模型对象
@@ -37,15 +40,52 @@ def save_model(model, transfer, pkl_path):
         return
     if not pkl_path.endswith("/") or not pkl_path.endswith("\\"):
         pkl_path += "/"
-    m_path = pkl_path + "model.pkl"
-    transfer_path = pkl_path + "transfer.pkl"
 
+    m_path = pkl_path + "model.pkl"
     print("序列化... ")
     with open(m_path, "wb") as fw:
         pickle.dump(model, fw)
-    with open(transfer_path, "wb") as fw:
-        pickle.dump(transfer, fw)
+    if transfer is not None:
+        transfer_path = pkl_path + "transfer.pkl"
+        with open(transfer_path, "wb") as fw:
+            pickle.dump(transfer, fw)
     print("序列化完成")
+
+
+def eval_model_vec(train_data, test_data, model, pkl_path):
+    logging.info("---------------------------cut_off_begin--------------------------------")
+    logging.info("VEC TESTING function.")
+
+    if isinstance(train_data, str) and isinstance(test_data, str):
+        logging.info("<load " + train_data + " and " + test_data + ">")
+        train_X, train_Y = load_vecs(train_data)
+        test_X, test_Y = load_vecs(test_data)
+    else:
+        train_X, train_Y = train_data
+        test_X, test_Y = test_data
+        logging.info("<loading train data size:" + str(train_X.shape) + " and loading test data size:" + str(
+            test_X.shape) + ">")
+
+    logging.info(type(model))
+    logging.info("boundary:" + str(len(train_X)))
+
+    logging.info("building...")
+    model.fit(train_X, train_Y)
+    logging.info("building finished.")
+
+    if pkl_path is not None:
+        save_model(pkl_path, model)
+    logging.info("pkl finished.")
+
+    y_true, y_pred = test_Y, model.predict(test_X)
+    res = classification_report(y_true, y_pred)
+    logging.info(res)
+    res = precision_recall_fscore_support(y_true, y_pred)
+    logging.info(res)
+    res = confusion_matrix(y_true, y_pred)
+    logging.info(res)
+    logging.info("<<<[tuning end]>>>\n")
+    return res
 
 
 def eval_model(train_data, test_data, model, pkl_path, stop_words):
@@ -77,7 +117,7 @@ def eval_model(train_data, test_data, model, pkl_path, stop_words):
     logging.info("building finished.")
 
     if pkl_path is not None:
-        save_model(model, doc_term, pkl_path)
+        save_model(pkl_path, model, doc_term)
 
     y_true, y_pred = test_Y, model.predict(test_X)
     res = classification_report(y_true, y_pred)
@@ -87,6 +127,7 @@ def eval_model(train_data, test_data, model, pkl_path, stop_words):
     res = confusion_matrix(y_true, y_pred)
     logging.info(res)
     logging.info("<<<[tuning end]>>>\n")
+    return res
 
 
 def load_model(model_path):
@@ -107,6 +148,20 @@ def load_model(model_path):
     with open(transfer_path, "rb") as fr:
         transfer = pickle.load(fr)
     return model, transfer
+
+
+def load_model_vec(model_path):
+    """
+    模型反序列化
+    :param model_path: 模型文件路径
+    :return: 模型
+    """
+    if not os.path.exists(model_path):
+        print("模型路径非法")
+        return
+    with open(model_path, "rb") as fr:
+        model = pickle.load(fr)
+    return model
 
 
 def predict(docs, model_path, model=None, transfer=None):
